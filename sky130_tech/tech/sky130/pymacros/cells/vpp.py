c80292db8a5b2aac75bcebb269c07b524191dc76
Amro Tork <74936860+atorkmabrains@users.noreply.github.com>
2023-08-28 12:20:06 +0000

    - Updating mabrains PCells licenses.

--- sky130_tech/tech/sky130/pymacros/cells/vpp.py
+++ sky130_tech/tech/sky130/pymacros/cells/vpp.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Efabless Corporation
+# Copyright 2022 Mabrains LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
